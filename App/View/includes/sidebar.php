1b29827dad9b73d01509e1a9643a3823dffa9744
juninho chiodi <juninho.junirj@gmail.com>
2023-09-28 15:57:20 -0300

    include da side bar

---
+++ App/View/includes/sidebar.php
@@ -0,0 +1,32 @@
+<div class="sidebar" style="align-self: center;">
+        <div class="centered">
+            <img src="/View/Assets/logo.png" alt="Logo" width="150px" ; height="150px" ;>
+        </div>
+        <!--<a href="#">Home</a>-->
+
+        <div class="centered">
+            <svg xmlns="http://www.w3.org/2000/svg" style="margin-right: 5px;" width="25" height="25" fill="#884A39" class="bi bi-bag-fill" viewBox="0 0 16 16">
+                <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 
+                    3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5z" />
+            </svg>
+            <b style="font-size: 20px; text-align: center; margin: 05px 0px 0px 0px; 
+                    color: #884A39; font-family: Boulder, Sans Serif;"> LOJA FÍSICA</b>
+        </div>
+
+        <a href="#">Vendas</a>
+        <a href="#">Controle de Vendas</a>
+
+        <div class="centered" style="margin-top: 5px;">
+            <img src="/View/Assets/home.png" style="margin-right: 5px;" alt="Logo" width="28px" ; height="28px" ;>
+            <b style="font-size: 20px; text-align: center; margin: 05px 0px 0px 0px; 
+                color: #884A39; font-family: Boulder, Sans Serif;">SERVIÇOS</b>
+        </div>
+
+        <a href="#">Cadastros</a>
+        <a href="#">Agendamento</a>
+        <a href="#">Hotelzinho</a>
+
+        <div class="centered">
+            <img src="/View/Assets/doguinho.png" alt="Logo" width="90px" ; height="90px" ; style="margin: 5px; position: absolute; bottom:10px;">
+        </div>
+    </div>
